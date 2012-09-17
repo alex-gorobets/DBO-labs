6b8558550f39e87c7e36265bf09ab22ae7180b90
Bohdan Nanovskyi <bnanovsky@gmail.com>
2012-09-17 21:18:42 +0300

    GitHub + Eclipse test

---
+++ DBO_Dream_Team/Git_Test.py
@@ -0,0 +1,17 @@
+'''
+Created on 17 вер. 2012
+
+@author: Bohdan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
